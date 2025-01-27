9643f88ea5266f46162ffcc2d30df29e51f321cb
Kumar Manish <mailme.manishverma@gmail.com>
2025-01-27 11:41:07 +0530

    Basic Authentication with In-memory user

---
+++ src/main/java/com/manish/springsecuritybasics/config/MySecurityConfig.java
@@ -0,0 +1,72 @@
+package com.manish.springsecuritybasics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Configuration class for Spring Security.
+ */
+@Configuration
+public class MySecurityConfig {
+
+    /**
+     * Configures an in-memory user details service with a single user.
+     *
+     * @return the user details service
+     */
+    @Bean
+    UserDetailsService userDetailsService() {
+        // Create an in-memory user details manager
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        // Define a user with username "manish", password "manish" (encoded using BCrypt), and authority "read"
+        UserDetails userDetail = User
+                .withUsername("manish")
+                .password(this.passwordEncoder().encode("manish"))
+                .authorities("read")
+                .build();
+
+        // Add the user to the in-memory user details manager
+        userDetailsService.createUser(userDetail);
+        return userDetailsService;
+    }
+
+    /**
+     * Configures a password encoder bean.
+     *
+     * @return the password encoder
+     */
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        // Return a BCrypt password encoder
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Configures the security filter chain.
+     *
+     * @param http the HttpSecurity object
+     * @return the security filter chain
+     * @throws Exception if an error occurs
+     */
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // Ensure that all requests are authenticated
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                // Use HTTP Basic authentication with default settings
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/manish/springsecuritybasics/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.manish.springsecuritybasics.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Spring Security Rocks", HttpStatus.OK);
+    }
+}
